32b48188d33a7f14ac5f3e65d01a7e01490ef4cd
viraftryn <fitriyanivira03@gmail.com>
2026-06-09 15:54:47 +0700

    update portfolio view
    tapable stocks list in portfolio view to stock detail view

--- SahamIndo/SahamIndo/PortfolioView.swift
+++ SahamIndo/SahamIndo/PortfolioView.swift
@@ -294,7 +294,9 @@ struct PortfolioView: View {
                                 }
                             }
                             .padding(.vertical, 10)
-                            
+                            .contentShape(Rectangle())
+                            .onTapGesture { router.push(.stockDetail(item)) }
+
                             if item.symbol != activePositions.last?.symbol {
                                 Divider().background(Color.primary.opacity(0.08))
                             }
